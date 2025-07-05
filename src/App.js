8391f98eb98fefff88614066ea1f629ba7033a9b
David Sy <davidsy2013@gmail.com>
2025-07-04 21:01:05 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <h1>Mimi's Bakery & Floral Shops</h1> */}
+        <h1>Mimi's Bakery and Floral Shop</h1>
+        <p>Fresh baked goods and beautilful flowers</p>
+        <button className="order-button">Order Now</button>
       </header>
     </div>
   );
